031fb9bc53b82af8cb57a04c60dd7c0b34cdfead
Utpal29 <utpal29flash@gmail.com>
2020-09-28 20:31:33 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//  Search input
+const searchUser = document.getElementById('searchUser');
+
+// Search input event listner
+searchUser.addEventListener('keyup', (e) => {
+    // Get input text
+    const userText = e.target.value;
+
+    if(userText !== ''){
+
+    }
+});
