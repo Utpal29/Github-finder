506e1aedaf6e4918c2ed3161e02f4a3edb1ed551
Utpal Prajapati <54059872+Utpal29@users.noreply.github.com>
2020-09-29 19:21:01 +0530

    Update github.js

--- github.js
+++ github.js
@@ -1,31 +1,22 @@
 class Github {
-    constructor() {
-        this.config = {
-            headers: {
-                Authorization: 'token 27984621817ad32a03bf66e5de032af6a9591acd '
-            }
-        }
-        this.repos_count = 5
-        this.repos_sort = 'created: asc'
+  constructor() {
+    this.client_id = "3b925c08aab40ac38d05";
+    this.client_secret = "31be1b1285e20f238e8777adb3bc11907739a47b";
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
     }
-    async getUser(user) {
-        const profileResponse = await fetch(
-            `https://api.github.com/users/${user}`,
-            this.config
-        )
-
-        const repoResponse = await fetch(
-            `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`,
-            this.config
-        )
-
-        const profile = await profileResponse.json()
-
-        const repos = await repoResponse.json()
-
-        return {
-            profile,
-            repos
-        }
-    }
+  }
 }
