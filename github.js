6b5a0f465aab3faf66cf2ab2881ae39728e373f5
Utpal29 <utpal29flash@gmail.com>
2020-09-29 17:46:44 +0530

    file added

--- github.js
+++ github.js
@@ -1,3 +1,31 @@
-class GitHub{
-    
+class Github {
+    constructor() {
+        this.config = {
+            headers: {
+                Authorization: 'token 27984621817ad32a03bf66e5de032af6a9591acd '
+            }
+        }
+        this.repos_count = 5
+        this.repos_sort = 'created: asc'
+    }
+    async getUser(user) {
+        const profileResponse = await fetch(
+            `https://api.github.com/users/${user}`,
+            this.config
+        )
+
+        const repoResponse = await fetch(
+            `https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}`,
+            this.config
+        )
+
+        const profile = await profileResponse.json()
+
+        const repos = await repoResponse.json()
+
+        return {
+            profile,
+            repos
+        }
+    }
 }
